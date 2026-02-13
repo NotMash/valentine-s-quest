0b037a192c0a946a005f99f4f1847a8c9d920eed
Ibrahim Miah <75740933+NotMash@users.noreply.github.com>
2026-02-13 01:44:41 +0000

    Revert layout wrapper

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,7 @@
 import ValentineGame from "../components/game/ValentineGame";
 
 const Index = () => {
-  return (
-    <div className="min-h-[100svh] w-full bg-slate-900 flex justify-center">
-      <div className="relative w-full max-w-[420px] h-[100svh]">
-        <ValentineGame girlfriendName="Shahina" />
-      </div>
-    </div>
-  );
+  return <ValentineGame girlfriendName="Shahina" />;
 };
 
 export default Index;
